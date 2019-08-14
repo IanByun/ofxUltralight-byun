dbb4fdc15a56d75d09db01c33405d86d83144f16
JungHyun Ian Byun <innoian92@gmail.com>
2019-08-14 14:01:01 +0900

    rbuf_texture_id PBO로 빠르게 읽기

--- example-ultralight/src/ofApp.cpp
+++ example-ultralight/src/ofApp.cpp
@@ -4,33 +4,80 @@
 void ofApp::setup() {
 	web_loader = make_shared<ofxUltralight>();
 
-	web_loader->requestCreateView(
-		960, 540, "https://m.search.naver.com/search.naver?query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%8B%9C%EA%B3%84");
-	web_loader->requestCreateView(
-		960, 540, "https://m.weather.naver.com/m/main.nhn?regionCode=09140104&lang=en");
-	web_loader->requestCreateView(
-		960, 540, "https://www.google.com/search?q=calculator");
-	web_loader->requestCreateView(
-		960, 540, "https://www.apple.com/kr/iphone/");
+	//web_loader->requestCreateView(960, 540, "https://m.search.naver.com/search.naver?query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%8B%9C%EA%B3%84");
+	//web_loader->requestCreateView(960, 540, "https://m.weather.naver.com/m/main.nhn?regionCode=09140104&lang=en");
+	web_loader->requestCreateView(960, 540, "https://www.google.com/search?q=calculator");
+	//web_loader->requestCreateView(960, 540, "https://www.apple.com/kr/iphone/");
+
+	// screen quad VAO
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * quadVertices.size(), quadVertices.data(), GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	web_loader->update();
+	//web_loader->update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 	ofClear(ofColor::black);
 
+	//for (int i = 0; i < 100; i++)
+		web_loader->update();
+
 	auto web_assets = web_loader->getViewAssets();
 
-	if (web_assets.size() == 4) {
+	auto driver = static_cast<GPUDriverGL*>(web_loader->gpu_driver);
+
+	auto buffer_map = driver->GetFrameBuffer();
+
+	if (buffer_map.size() != 0) {
+		GLint fbo_id = buffer_map.begin()->first;
+		GLint tex_id = buffer_map.begin()->second;
+
+		if (false) { //easy, naive, slow
+			glBindTexture(GL_TEXTURE_2D, tex_id);
+			glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)frame_data.data());
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+		else {
+			ReadTexture(tex_id, pbo_id, frame_data);
+		}
+
+		cv::Mat bmat = cv::Mat(540, 960, CV_8UC4, (void*)frame_data.data());
+		cv::cvtColor(bmat, bmat, cv::COLOR_RGBA2BGR);
+
+		cv::imshow("ast.mat", bmat);
+		cv::waitKey(1);
+
+		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
+		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		//glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+
+		//// clear all relevant buffers
+		//glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
+		//glClear(GL_COLOR_BUFFER_BIT);
+
+		//glBindVertexArray(quadVAO);
+		//glBindTexture(GL_TEXTURE_2D, tex_id);	// use the color attachment texture as the texture of the quad plane
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+	}
+
+	/*if (web_assets.size() == 4) {
 		web_assets[0].tex.draw(0, 0, 960, 540);
 		web_assets[1].tex.draw(960, 0, 960, 540);
 		web_assets[2].tex.draw(0, 540, 960, 540);
 		web_assets[3].tex.draw(960, 540, 960, 540);
-	}
+	}*/
 }
 
 //--------------------------------------------------------------
--- example-ultralight/src/ofApp.h
+++ example-ultralight/src/ofApp.h
@@ -24,4 +24,48 @@ class ofApp : public ofBaseApp {
 		void gotMessage(ofMessage msg);
 		
 		ofxUltralight::Ptr web_loader;
+		ofFbo		perspectiveFrame;
+
+		unsigned int quadVAO, quadVBO;
+		vector<float> quadVertices = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+			// positions   // texCoords
+			0.0f,  500.0f,  0.0f, 1.0f,
+			0.0f, 0.0f,  0.0f, 0.0f,
+			1000.0f, 0.0f,  1.0f, 0.0f,
+
+			0.0f,  500.0f,  0.0f, 1.0f,
+			1000.0f, 0.0f,  1.0f, 0.0f,
+			1000.0f,  500.0f,  1.0f, 1.0f
+		};
+
+		const int width = 960, height = 540;
+		const std::vector<BYTE> frame_data = std::vector<BYTE>(width * height * 4);
+		
+		GLuint GenerateReadPBO(int width, int height) {
+			GLuint  pbo;
+			glGenBuffers(1, &pbo);
+			glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
+			glBufferData(GL_PIXEL_PACK_BUFFER, width*height * 4, NULL, GL_STREAM_READ);
+			glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+			return pbo;
+		}
+		
+		void ReadTexture(GLuint tex_id, GLuint pbo_id, OUT const std::vector<BYTE>& pixel_data) {
+			glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo_id);
+			glBindTexture(GL_TEXTURE_2D, tex_id);
+			glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+				(GLvoid*)0 // offset in bytes into "buffer", not pointer to client memory!
+			);
+			glBufferData(GL_PIXEL_PACK_BUFFER, pixel_data.size(), 0, GL_STREAM_READ); //GL_STREAM_READ ¸»°í GL_STATIC_READ ÇÏ¸é ±ôºý±ôºý!
+
+			GLubyte* dma_ptr = (GLubyte*)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+			if (dma_ptr) { 	// update data directly on the mapped buffer
+				memcpy((void*)pixel_data.data(), dma_ptr, 960 * 540 * 4);
+				glUnmapBuffer(GL_PIXEL_PACK_BUFFER); // release the mapped buffer
+			}
+			glBindTexture(GL_TEXTURE_2D, 0);
+			glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+		}
+
+		const GLuint pbo_id = GenerateReadPBO(width, height);
 };
